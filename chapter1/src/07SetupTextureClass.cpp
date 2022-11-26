4963f7c530b50e5e43c90d7b38c27f11a757e4b3
jasmineyg <1023335800@qq.com>
2022-11-26 09:59:43 +0800

    纹理1彩色木箱子

--- chapter1/src/07SetupTextureClass.cpp
+++ chapter1/src/07SetupTextureClass.cpp
@@ -14,14 +14,14 @@ auto pass_geometry_data_to_GPU(float vertices_array[], int vertices_array_size, 
  */
 float vertices[] = {
         // positions                   // colors                      // texture coords
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom left
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, // bottom left
         -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,   //top left
-        0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f   // top right
+        0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f   // top right
 };
 unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,// first Triangle
-        1,2,3
+        3,0,2,// first Triangle
+        0,1,2
 };
 
 /**
